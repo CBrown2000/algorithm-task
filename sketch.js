fb2a19eb5c3c5536befa91e939235cc9f641d78b
CBrown2000 <146858039+CBrown2000@users.noreply.github.com>
2024-02-01 14:32:09 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,37 @@
+function setup() {
+    const canvasSize = 800;
+    const squareSize = 50;
+    const height = squareSize;
+    const width = squareSize
+    const cellNumber = 10;
+    const cellSize = 50;
+
+
+    createCanvas(800, 800);
+
+    let x = 0;
+    let y = 0;
+
+    while(y < canvasSize) {
+        x = 0
+        const borderColor = color("#804040");
+        stroke(borderColor);
+        strokeWeight(5);
+        
+    }
+
+    
+
+
+
+
+    
+
+
+}
+
+function draw() {
+    background(220);
+
+
+}
