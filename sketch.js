dd00f2d06d42da73c74fbfd60b80bb6fd16e2a9a
CBrown2000 <146858039+CBrown2000@users.noreply.github.com>
2024-02-01 16:00:45 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,37 +1,75 @@
+x = 0
+y = 0
+let squareSize = 100;
+let dir = 1;
+
 function setup() {
-    const canvasSize = 800;
-    const squareSize = 50;
-    const height = squareSize;
-    const width = squareSize
-    const cellNumber = 10;
-    const cellSize = 50;
 
 
-    createCanvas(800, 800);
+// set canvas size
+    const canvasSize = 700;
 
-    let x = 0;
-    let y = 0;
 
-    while(y < canvasSize) {
-        x = 0
-        const borderColor = color("#804040");
-        stroke(borderColor);
-        strokeWeight(5);
-        
-    }
+
+    createCanvas(700, 700);
+
+
+
+    background(220);
+for (let i = 0; i < width; i += 100) {
+    for (let j = 0; j < height; j += 100) {
+      rect(i, j, 100, 100);
+      rect(i,j,95,95)
+      rect(i,j,90,90)
+      rect(i,j,85,85)
+      rect(i,j,80,80)
+      rect(i,j,75,75)
+      rect(i,j,70,70)
+      rect(i,j,65,65)
+      rect(i,j,60,60)
+    };
+
+
+
+
+
 
     
+}
+
+    let d = 100; // initial line is 100px
+
+    //draw new line form end point that is rotated between 90 or 91 or 92 degrees
+    //new line is always alittle shorter, minus px length
+
+    //continue till square like spiral forms
 
 
+    // rect(x,y,squareSize,squareSize) //draw rectangle
 
-
+    // x += dir*squareSize;
+    // y += dir*squareSize;
     
-
+    //  // Change direction every 10 frames
+    //  if (frameCount % 10 == 0) {
+    //     dir *= -1;
+    //     squareSize -= 5; // Decrease the size of the square
+    //  }
+      
+    //  // Stop drawing after the square gets too small
+    //  if (squareSize <= 0) {
+    //     noLoop();
+    //  }
+    
 
 }
 
+
+
+// draw a grid using a nested for loop
 function draw() {
-    background(220);
+
+
 
 
 }
