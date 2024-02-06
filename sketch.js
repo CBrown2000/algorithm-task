b9807a0f0c960e709c282dc4438d9d8f30d77563
CBrown2000 <146858039+CBrown2000@users.noreply.github.com>
2024-02-06 09:43:07 +0000

    updated

--- sketch.js
+++ sketch.js
@@ -13,36 +13,36 @@ function setup() {
 
 
     createCanvas(700, 700);
+    let rotationAngles = [];
 
 
 
     background(220);
     for (let i = 0; i < width; i += 100) {
         for (let j = 0; j < height; j += 100) {
-
-
             rect(i, j, 100, 100); //main grid square
+            push();
 
-            let newi = i + 5
-            let newj = j + 5
 
-            rect(newi, newj, 90, 90,)
+            let newi = i + 5;
+            let newj = j + 5;
+            rect(newi, newj, 90, 90,);
+            
+            let newi1 = i + 10;
+            let newj1 = j + 10;
+            rect(newi1, newj1, 80, 80);
 
-            let newi1 = i + 10
-            let newj1 = j + 10
-            rect(newi1, newj1, 80, 80)
+            let newi2 = i + 15;
+            let newj2 = j +15;
+            rect(newi2, newj2, 70, 70);
 
-            let newi2 = i + 15
-            let newj2 = j +15
-            rect(newi2, newj2, 70, 70)
+            let newi3 = i + 20;
+            let newj3 = j + 20;
+            rect(newi3, newj3, 60, 60);
 
-            let newi3 = i + 20
-            let newj3 = j + 20
-            rect(newi3, newj3, 60, 60)
-
-            let newi4 = i + 25
-            let newj4 = j + 25
-            rect(newi4, newj4, 50, 50)
+            let newi4 = i + 25;
+            let newj4 = j + 25;
+            rect(newi4, newj4, 50, 50);
 
             
 
@@ -59,34 +59,13 @@ function setup() {
 
     let d = 100; // initial line is 100px
 
-    //draw new line form end point that is rotated between 90 or 91 or 92 degrees
-    //new line is always alittle shorter, minus px length
-
-    //continue till square like spiral forms
-
-
-    // rect(x,y,squareSize,squareSize) //draw rectangle
-
-    // x += dir*squareSize;
-    // y += dir*squareSize;
-
-    //  // Change direction every 10 frames
-    //  if (frameCount % 10 == 0) {
-    //     dir *= -1;
-    //     squareSize -= 5; // Decrease the size of the square
-    //  }
-
-    //  // Stop drawing after the square gets too small
-    //  if (squareSize <= 0) {
-    //     noLoop();
-    //  }
 
 
 }
 
 
 
-// draw a grid using a nested for loop
+
 function draw() {
 
 
